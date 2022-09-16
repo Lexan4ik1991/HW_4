7e67a5b4b573fdd1beede171761f0accf5132a63
Alexey <112006201+Lexan4ik1991@users.noreply.github.com>
2022-09-16 14:59:25 +0400

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+void SummNumber(int num)
+{
+while (num > 0)
+{
+int result = num % 10;
+num = num / 10;
+summa = summa + result;
+}
+Console.WriteLine("сумма всех цифр равна: " + summa);
+}
+SummNumber(num);
---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = new int[8];
+FillArray(array);
+PrintArray(array); 
+void FillArray(int[] array)
+{
+int length = array.Length; 
+int index = 0; 
+    while(index < length)
+    {
+     array[index] = new Random().Next(-8,9); 
+     index++; 
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length; 
+    int index = 0; 
+    while(index < length)
+    {
+        Console.Write($"{array[index]}, ");  
+        index++;     
+    }
+}
