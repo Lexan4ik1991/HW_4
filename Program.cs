dceb2da692aaba028704b5e4c5681bbcbe06ef44
Lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-09-16 14:34:19 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int num = numberA;
+int i=0;
+void Stepen (int num)
+{
+   for (i=1; i<numberB; i++)
+   {
+    num*=numberA;
+   }
+Console.WriteLine("A в степени B равно: " + num);
+}
+Stepen(num);
